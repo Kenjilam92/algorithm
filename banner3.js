49e3396f72cd5cd1f15755df1a076bcddfd96b99
kenjilam92 <kenji.lam.92@gmail.com>
2020-07-31 21:36:57 -0400

    banner

--- banner3.js
+++ banner3.js
@@ -1,3 +1,16 @@
+ex1=[3,1,4];
+ex2=[5,3,2,4];
+ex3=[5, 3, 5, 2, 1];
+ex4=[7, 7, 3, 7, 7];
+ex5=[1, 1, 7, 6, 6, 6];
+ex6=[1,2,3,3,5,2];
+ex7=[1,1,2,3,3,5,2];
+console.log(solution(ex1));
+console.log(solution(ex2));
+console.log(solution(ex3));
+console.log(solution(ex4));
+console.log(solution(ex5));
+
 function solution(H,Total=null) {
     // write your code in JavaScript (Node.js 8.9.4)
     if (Total===null){
@@ -17,10 +30,38 @@ function solution(H,Total=null) {
     let R = H[1];
     let W = 2;
     let i = 2;
-    let isEqualHead = true;
     if (L===R){
+        let EqualGap = 2;
         while(i<H.length){
-            
+            if (R===H[i] && L===H[i]){
+                W++;
+                i++;
+                EqualGap++;
+            }
+            else if(L>H[i]){
+                R=H[i];
+                W++;
+                i++;
+            }
+            else if (L===H[i]){
+                W++;
+                if(i===H.length-1){
+                    R=H[i];
+                    Total += Calculation(L,R,W); 
+                    return Total;
+                }
+                else if(L!==H[i+1]){
+                    R=H[i];
+                    i++;
+                    Total += Calculation(L,R,W); 
+                    return solution(H.slice(i,H.length),Total);
+                }
+                i++;
+            }
+            else {
+                Total+=L*W;
+                return solution(H.slice(i,H.length),Total);
+            }
         }
         Total+=CalculationEnd(L,R,W);
         return Total;
@@ -34,10 +75,10 @@ function solution(H,Total=null) {
                 }
                 else if (L===H[i]){
                     W++;
-                    if(i=H.length-1){
+                    if(i===H.length-1){
                         R=H[i];
                         Total += Calculation(L,R,W); 
-                        return Total
+                        return Total;
                     }
                     else if(L!==H[i+1]){
                         R=H[i];
@@ -45,6 +86,7 @@ function solution(H,Total=null) {
                         Total += Calculation(L,R,W); 
                         return solution(H.slice(i,H.length),Total);
                     }
+                    i++;
                 }
                 else {
                     R=H[i-1];
@@ -54,7 +96,6 @@ function solution(H,Total=null) {
             }
             else{//L<R
                 if (R<=H[i]){
-                    L=R
                     R=H[i];
                     W++;
                     i++;
